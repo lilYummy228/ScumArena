f9b223954578c8b641c2f182aa7132f154fff3df
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2025-06-17 17:00:42 +0500

    feat: add new logic of movement

--- Assets/Scripts/MovementStage.cs
+++ Assets/Scripts/MovementStage.cs
@@ -5,36 +5,46 @@ using UnityEngine;
 
 public class MovementStage : MonoBehaviour
 {
-    [SerializeField] private float _speed = 2;
+    [SerializeField] private float _speed = 2f;
 
     private WaitForFixedUpdate _waitForFixedUpdate = new WaitForFixedUpdate();
+    private float _timeToMove = 4f;
 
     public event Action MovementStageFinished;
+
+    private WaitForSeconds WaitForMoving => new WaitForSeconds(_timeToMove);
 
     public void MoveTo(IReadOnlyDictionary<Unit, Cell> unitsCell)
     {
         Debug.Log("Movement Stage Started");
 
-        StartCoroutine(SmoothlyMove(unitsCell));
+        bool[] isMoved = new bool[unitsCell.Count];
+
+        foreach (KeyValuePair<Unit, Cell> unitCellPair in unitsCell)
+            StartCoroutine(SmoothlyMove(unitCellPair));
+
+        StartCoroutine(AwaitMovement());
     }
 
-    public IEnumerator SmoothlyMove(IReadOnlyDictionary<Unit, Cell> unitsCell)
+    private IEnumerator AwaitMovement()
     {
-        foreach (KeyValuePair<Unit, Cell> unitCellPair in unitsCell)
-        {
-            Vector3 targetPosition = new Vector3(unitCellPair.Value.transform.position.x, unitCellPair.Key.transform.position.y, unitCellPair.Value.transform.position.z);
-
-            while (unitCellPair.Key.transform.position != targetPosition)
-            {
-                unitCellPair.Key.transform.position = Vector3.MoveTowards(unitCellPair.Key.transform.position, targetPosition, _speed * Time.deltaTime);
-                yield return _waitForFixedUpdate;
-            }
-
-            unitCellPair.Key.SetCoordinates(unitCellPair.Value.Coordinates.x, unitCellPair.Value.Coordinates.y);
-        }
+        yield return WaitForMoving;
 
         MovementStageFinished?.Invoke();
 
         Debug.Log("Movement Stage Finished");
+    }
+
+    private IEnumerator SmoothlyMove(KeyValuePair<Unit, Cell> unitCellPair)
+    {
+        Vector3 targetPosition = new Vector3(unitCellPair.Value.transform.position.x, unitCellPair.Key.transform.position.y, unitCellPair.Value.transform.position.z);
+
+        while (unitCellPair.Key.transform.position != targetPosition)
+        {
+            unitCellPair.Key.transform.position = Vector3.MoveTowards(unitCellPair.Key.transform.position, targetPosition, _speed * Time.deltaTime);
+            yield return _waitForFixedUpdate;
+        }
+
+        unitCellPair.Key.SetCoordinates(unitCellPair.Value.Coordinates.x, unitCellPair.Value.Coordinates.y);
     }
 }
