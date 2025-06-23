a9256741f3784c091adcdc3a5c4174d5e50a2c07
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2025-06-23 13:58:35 +0500

    chore

--- Assets/Scripts/Cells/CellAbilities/CellAbilityBase.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public abstract class CellAbilityBase : MonoBehaviour
-{
-    [SerializeField] private CellAbilityProperties _properties;
-
-    public abstract void Action();
-}
