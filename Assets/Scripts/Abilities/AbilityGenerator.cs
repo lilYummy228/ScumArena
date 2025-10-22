f83a682b66bf98ba084cb2787b2783fed027a3e3
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2025-10-22 14:12:08 +0500

    feat: add ability generator logic

--- Assets/Scripts/Abilities/AbilityGenerator.cs
+++ Assets/Scripts/Abilities/AbilityGenerator.cs
@@ -1,17 +1,22 @@
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class AbilityGenerator : MonoBehaviour
 {
     [SerializeField] private Ability[] _availableAbilities;
     [SerializeField] private Grid _grid;
 
-    private List<Ability> _spawnedAbilities;
+    private Dictionary<Ability, int> _spawnedAbilitiesX = new Dictionary<Ability, int>();
 
     public void SpawnAbility()
     {
-        _spawnedAbilities.Clear();
+        _spawnedAbilitiesX.Clear();
 
-
+        for (int i = 0; i < _grid.GridSizeX; i++)
+            _spawnedAbilitiesX.Add(GetRandomAbility(), i);
     }
+
+    private Ability GetRandomAbility() =>
+        _availableAbilities[Random.Range(0, _availableAbilities.Length - 1)];
 }
--- Assets/Scripts/Grid/Grid.cs
+++ Assets/Scripts/Grid/Grid.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-    [SerializeField] private List<Cell> _cells = new();
+    [SerializeField] private List<Cell> _cells;
 
     private Vector2Int _gridSize;
 
     public IReadOnlyList<Cell> CellsInGrid => _cells;
+    public int GridSizeX => _gridSize.x;
 
     public void SetGrid(List<Cell> cells, Vector2Int size)
     {
